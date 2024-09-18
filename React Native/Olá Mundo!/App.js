a3d8632e356ea3ca9c772620407854c355be3b27
Cleison de Lima <71999753+cleisonlima@users.noreply.github.com>
2024-09-18 09:16:45 -0300

    Add files via upload

---
+++ React Native/Olá Mundo!/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import {Text, View} from 'react-native';// Importação de componentes
+
+const YourApp = () => {
+  return (
+    <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <Text>Olá Mundo!</Text>
+    </View>
+  );
+};
+
+export default YourApp
